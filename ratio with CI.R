74edc329bdcad13b787be83a287c1991e70bd4df
laurabiggins <laurabiggins10@gmail.com>
2021-04-14 14:31:33 +0100

    cleaned up old output

--- ratio with CI.R
+++
@@ -1,9 +0,0 @@
-setwd("//fs-home/segondsa/Work/Andrea")
-
-library(mratios)
-
-ratio.test <-read.csv("ratio t test example.csv")
-
-ttestratio(ratio.test$group1, ratio.test$group2, alternative="two.sided")
-
-
