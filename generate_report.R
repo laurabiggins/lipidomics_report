a92b86b669b4197604672ef7f3d147bb8c9c685f
laurabiggins <laurabiggins10@gmail.com>
2021-04-27 09:50:16 +0100

    Added more annotation to generate_report.R

--- generate_report.R
+++ generate_report.R
@@ -1,36 +1,43 @@
-source("R/utils.R")
-
 #========================================================================
 # Change the options below to the correct file names and paths
-# examples of data files can be seen here in the GitHub repository here:
+# examples of data files can be seen in the GitHub repository here:
 # https://github.com/laurabiggins/lipidomics_report
 #========================================================================
-#
-# the input dataset and metadata file - these need to be full paths and contain the 
-# file suffix e.g. .csv/.txt
-data_file_path <- "D:/projects/lipidomics/example_data/CSC/datasets/alldata.lipidomics_summary_MiaPaca2_Parental-CSC.csv"
-meta_file <- "D:/projects/lipidomics/example_data/CSC/metadata_4_reps.txt" 
 
-# name of the output file - this doesn't need be a full path or contain a file suffix
-outfile <- "MiaPaca2_Parental-CSC"
+# The input dataset and metadata file - these can be full or relative paths and 
+# must contain the file suffix e.g. .csv/.txt/.tsv
+data_file_path <- "data/alldata.lipidomics_summary_test_data.tsv"
+meta_file <- "data/metadata.txt"
 
-# whether the experimental design is matched/paired or not. TRUE or FALSE value required here.
-matched <- FALSE
+# Name of the output file - this doesn't need be a full path or contain a file suffix.
+# By default, a folder named "output" will be created in the directory containing 
+# the data_file and that will contain the output file.
+outfile <- "example_data"
+
+# Whether the experimental design is matched/paired or not. TRUE or FALSE value required here.
+# Explanation of independent vs matched design can be found here:
+# https://github.com/laurabiggins/lipidomics_report/blob/main/info/Experimental_design_independent_vs_matched.pdf
+matched_samples <- FALSE
 
 
 #==============================================
 # The code below should be run but not changed
 #==============================================
+source("R/utils.R")
 create_report(
-  data_file = data_file_path, 
-  metadata_file = meta_file, 
+  data_file = normalizePath(data_file_path), 
+  metadata_file = normalizePath(meta_file), 
   outfile_name = outfile, 
   matched = matched_samples
 )
 
 
+# Other example paths on my Windows machine
+# data_file_path <- "D:/projects/lipidomics/example_data/CSC/datasets/alldata.lipidomics_summary_MiaPaca2_Parental-CSC.csv"
+# meta_file <- "D:/projects/lipidomics/example_data/CSC/metadata_4_reps.txt" 
 
 
 # for multiple files
 # data_folder <- "D:/projects/lipidomics/example_data/CSC/datasets/" 
 # file_paths <- list.files(data_folder,full.names = TRUE)
+# loop through these
