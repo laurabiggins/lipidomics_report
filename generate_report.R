747848c34bec37127d91d9ab2ad72804faa68026
laurabiggins <laurabiggins10@gmail.com>
2021-05-21 09:49:02 +0100

    Added config file instead of specifying options in R script.

--- generate_report.R
+++ generate_report.R
@@ -1,82 +1,84 @@
-#========================================================================
-# Change the options below to the correct file names and paths
-# examples of data files can be seen in the GitHub repository here:
-# https://github.com/laurabiggins/lipidomics_report
-#========================================================================
+# This is the script called by lipidomics_report_wrapper.R
 
-# required packages that aren't explicitly loaded
-# tools, assertthat, rstatix, knitr, DT, plotly, pheatmap, rmarkdown
+# required packages that aren't all explicitly loaded
 packages_required <- c("tidyverse", "ggplot2", "tools", "assertthat", "rstatix",
                        "knitr", "DT", "plotly", "pheatmap", "rmarkdown", "RColorBrewer")
 if(!all(packages_required %in% rownames(installed.packages()))) {
-  print(paste0(
-    "please install the package ", 
-    packages_required[!packages_required %in% rownames(installed.packages())]
+  stop(
+    paste0(
+      "installation required for package(s) ", 
+      packages_required[!packages_required %in% rownames(installed.packages())]
   ))
-} else { print("Looks like all the required packages are installed, please continue...")}
+}
+
+config <- switch(tools::file_ext(config_file), 
+                 tsv = ,
+                 txt = readr::read_tsv(config_file, col_names = FALSE),
+                 csv = readr::read_csv(config_file, col_names = FALSE),
+                 stop("Unknown file extension on config file")
+)
+
+config <- tibble::deframe(config)
+
+expected_config_fields <- c(
+  "data_file_path", 
+  "metadata_file", 
+  "outfile", 
+  "control", 
+  "test_run",  
+  "matched_samples", 
+  "bar_class_ylabel"
+)
+
+# Validity checks
+assertthat::validate_that(
+  all(names(config) %in% expected_config_fields),
+  msg = paste0(
+    "Unexpected parameter name found in config file. The culprit is ", 
+    paste0(names(config)[!(names(config) %in% expected_config_fields)], collapse = ", "),
+    ". This/these will be ignored."
+  )  
+)
+
+assertthat::assert_that(file.exists(config[["data_file_path"]]))
+
+assertthat::assert_that(file.exists(config[["metadata_file"]]))
+
+assertthat::assert_that(
+  nchar(config[["outfile"]]) >= 1, 
+  msg = "outfile name must be supplied"
+)
+assertthat::assert_that(
+  nchar(config[["control"]]) >= 1, 
+  msg = "Name of control condition must be supplied"
+)
+assertthat::validate_that(
+  nchar(config[["bar_class_ylabel"]]) > 0,
+  msg = paste0("bar_class_ylabel value not found, will be left blank on plots")
+)
+
+# everything gets read in as characters so these need converting to logical
+assertthat::assert_that(
+  config[["matched_samples"]] %in% c("TRUE", "FALSE"),
+  msg = paste0("matched_samples option must be TRUE or FALSE, value found = ", config[["matched_samples"]])
+)
+matched_samples <- as.logical(config[["matched_samples"]])
+
+assertthat::assert_that(
+  config[["test_run"]] %in% c("TRUE", "FALSE"),
+  msg = paste0("quick_test option must be TRUE or FALSE, value found = ", config[["test_run"]])
+)
+test_run <- as.logical(config[["test_run"]])
 
 
-# The input dataset and metadata file - these can be full or relative paths and 
-# must contain the file suffix e.g. .csv/.txt/.tsv
-#data_file_path <- "data/alldata.lipidomics_summary_test_data.tsv"
-#meta_file <- "data/metadata.txt"
-
-data_file_path <- "../example_data/20210512/alldata.lipidomics_summary.tsv"
-meta_file <- "../example_data/20210512/metadata.txt"
-
-
-# Name of the output file - this doesn't need be a full path or contain a file suffix.
-# By default, a folder named "output" will be created in the directory containing 
-# the data_file and that will contain the output file.
-#outfile <- "example_data"
-
-outfile <- "output"
-
-# Whether the experimental design is matched/paired or not. TRUE or FALSE value required here.
-# Explanation of independent vs matched design can be found here:
-# https://github.com/laurabiggins/lipidomics_report/blob/main/info/Experimental_design_independent_vs_matched.pdf
-#matched_samples <- FALSE
-
-matched_samples <- TRUE
-
-# To generate the full report test_run should be set to FALSE
-# If TRUE, it speeds up the report generation by only plotting 3 of the individual plots
-test_run <- TRUE
-
-# label for y axis in class bar plots, must be text surrounded by quotes.
-bar_class_ylabel <- "ng/ng DNA"
-
-# Name of "control" condition, this must match the name of the condition in the metadata file.
-control <- "Control"
-
-
-#==============================================
-# The code below should be run but not changed
-#==============================================
 library(magrittr)
 source("R/utils.R")
 create_report(
-  data_file = normalizePath(data_file_path), 
-  metadata_file = normalizePath(meta_file), 
-  outfile_name = outfile, 
+  data_file = normalizePath(config[["data_file_path"]]), 
+  metadata_file = normalizePath(config[["metadata_file"]]), 
+  outfile_name = config[["outfile"]], 
+  bar_class_ylabel = config[["bar_class_ylabel"]],
+  control = config[["control"]],
   quick_test = test_run,
-  matched = matched_samples,
-  bar_class_ylabel = bar_class_ylabel,
-  control = control
+  matched = matched_samples
 )
-
-
-# Other example paths on my Windows machine
-data_file_path <- "D:/projects/lipidomics/example_data/CSC/datasets/alldata.lipidomics_summary_MiaPaca2_Parental-CSC.csv"
-meta_file <- "D:/projects/lipidomics/example_data/CSC/metadata_4_reps.txt" 
-control <- "Parental"; outfile <- "miapaca_parental"
-#control <- "Stem"
-#outfile <- "miapaca_stem"
-test_run <- TRUE
-bar_class_ylabel <- "ng/ng DNA"
-matched_samples <- TRUE
-
-# for multiple files
-# data_folder <- "D:/projects/lipidomics/example_data/CSC/datasets/" 
-# file_paths <- list.files(data_folder,full.names = TRUE)
-# loop through these
---
+++ lipidomics_report_wrapper.R
@@ -0,0 +1,9 @@
+# examples of data files can be seen in the GitHub repository here:
+# https://github.com/laurabiggins/lipidomics_report
+#========================================================================
+
+
+#config_file <- "D:/projects/lipidomics/lipidomics_report/data/config.csv"
+config_file <- file.choose()
+
+source("generate_report.R")
